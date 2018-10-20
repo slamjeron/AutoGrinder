6cbe46bcc6dd991ee6f910e4bb8e1caf2d9a97ce
jeronangell <elder.angell@gmail.com>
2018-10-19 19:05:43 -0600

    added stuff

--- auto Grind/Gui2/editPage.py
+++ auto Grind/Gui2/editPage.py
@@ -2,14 +2,61 @@ import tkinter
 class editPage(tkinter.Frame):
     def __init__(self, parent):
         tkinter.Frame.__init__(self, parent)
-        btn = tkinter.Button(self, text='btn')
-        btn.grid(row=0, column=1)
+        lbl=tkinter.Label(self,text='editing lines')
+        lbl.grid(row=0,column=0)
+        lbl1 = tkinter.Label(self, text='action')
+        lbl1.grid(row=1, column=0)
+        self.recVar=tkinter.StringVar()
+        actionDrop=tkinter.OptionMenu(self, self.recVar, 'Same', 'Left Click', 'Right Click', 'Drag Start',
+                           'Drag Stop', 'Type', 'Color Stall', 'Color Condition')
+        actionDrop.grid(row=1, column=1)
+        actionDrop.config(width=10)
+        self.recVar.set('Same')
+        lbl2 = tkinter.Label(self, text='time delay')
+        lbl2.grid(row=1, column=2)
+        delayEnt=tkinter.Entry(self)
+        delayEnt.grid(row=1,column=3)
+        delayEnt.insert(0,'Same')
+        delayEnt.config(width=7)
+        lbl3 = tkinter.Label(self, text='Point X')
+        lbl3.grid(row=1, column=4)
+        pointXEnt = tkinter.Entry(self)
+        pointXEnt.grid(row=1, column=5)
+        pointXEnt.insert(0,'Same')
+        pointXEnt.config(width=6)
+        lbl3 = tkinter.Label(self, text='Point y')
+        lbl3.grid(row=1, column=6)
+        pointYEnt = tkinter.Entry(self)
+        pointYEnt.grid(row=1, column=7)
+        pointYEnt.insert(0, 'Same')
+        pointYEnt.config(width=6)
+        lbl4 = tkinter.Label(self, text='key event')
+        lbl4.grid(row=1, column=8)
+
+        keyEnt = tkinter.Entry(self)
+        keyEnt.grid(row=1, column=9)
+        keyEnt.insert(0, 'Same')
+        keyEnt.config(width=7)
+
+
+
+        editlines = tkinter.Entry(self)
+        editlines.grid(row=0, column=1, columnspan=3)
+        savebtn = tkinter.Button(self, text='Save')
+        savebtn.grid(row=2, column=2)
+
+        delbtn = tkinter.Button(self, text='Delete')
+        delbtn.grid(row=2, column=3)
+
+        cursorbtn = tkinter.Button(self, text='get Cursor')
+        cursorbtn.grid(row=2, column=0)
+        getkeyStrockbtn = tkinter.Button(self, text='get key stroke')
+        getkeyStrockbtn.grid(row=2, column=0)
+        getkeyStrockbtn = tkinter.Button(self, text='get point')
+        getkeyStrockbtn.grid(row=2, column=1)
         self.pack()
         self.parent = parent
         self.line = tkinter.StringVar()
-        spiner = tkinter.Spinbox(self, from_=0, to=10000000, textvariable=self.line)
-        spiner.config(width=4)
-        spiner.grid(row=0, column=0)
 
         parent.protocol("WM_DELETE_WINDOW", self.onclose)
         self.pageUp = True
@@ -27,3 +74,7 @@ class editPage(tkinter.Frame):
 
     def pageClosed(self):
         return
+if __name__ == '__main__':
+    playcon = tkinter.Tk()
+    edit = editPage(playcon)
+    edit.mainloop()
--- auto Grind/Gui2/main.py
+++ auto Grind/Gui2/main.py
@@ -1,5 +1,4 @@
 import tkinter
-from scrole.vScroll import VerticalScrolledFrame
 from Gui2.MainControler import playControls
 
 
@@ -9,20 +8,19 @@ class mainPage(tkinter.Tk):
     def __init__(self, playcon):
         tkinter.Tk.__init__(self)
         self.recordingBox = tkinter.Listbox(self)
+        playcon.taskBox=self.recordingBox
+        playcon.main=self
         self.addTopButtons(playcon)
         self.config(height=200, width=400)
         self.pack_propagate(0)
-
         self.recordingBox.config(selectmode=tkinter.EXTENDED)
-
         scrollbar = tkinter.Scrollbar(self.recordingBox, orient="vertical")
         self.recordingBox.config(yscrollcommand=scrollbar.set)
         scrollbar.config(command=self.recordingBox.yview)
         scrollbar.pack(side="right", fill="y")
         self.recordingBox.pack(expand=1, fill=tkinter.BOTH)
 
-        for i in range(2):
-            self.recordingBox.insert(i,str(i))
+        playcon.taskBox = self.recordingBox
 
         bottomControls = tkinter.Frame(self)
         bottomControls.pack()
@@ -36,30 +34,35 @@ class mainPage(tkinter.Tk):
         nextPage.grid(row=2, column=2)
         self.bottomPage()
         self.title('Auto Grinder')
-        self.plBtnTxt = tkinter.StringVar()
-
+        playcon.showList()
         self.mainloop()
-    def ctrlcomand(self,key):
-        print(key)
-        self.recordingBox.config(selectmode = tkinter.MULTIPLE)
 
     def addTopButtons(self, playCon):
+        test = "play"
         btnPane = tkinter.Frame(self)
         btnPane.pack()
         recordBtn = tkinter.Button(btnPane, text='record', command=playCon.record)
         recordBtn.grid(row=0, column=0)
-        self.plBtnTxt=tkinter.StringVar()
-        self.startBtn = tkinter.Button(btnPane, textvariable=self.plBtnTxt, command=playCon.play)
-        self.plBtnTxt.set('Play')
-        self.startBtn.grid(row=0, column=2)
-        pauseBtn = tkinter.Button(btnPane, text='pause', command=playCon.pause)
-        pauseBtn.grid(row=0, column=3)
+        startBtn = tkinter.Button(btnPane, text=test, command=playCon.play)
+        startBtn.grid(row=0, column=2)
+
+        def setStartBTNText(text):
+            startBtn["text"] = text
+
+        def getStartBTNText():
+            return startBtn['text']
+
+        playcon.setStartBTNText = setStartBTNText
+        playcon.getStartBTNText = getStartBTNText
+
+
         stopBtn = tkinter.Button(btnPane, text='stop', command=playCon.stop)
         stopBtn.grid(row=0, column=1)
         delBtn = tkinter.Button(btnPane, text='delete', command=playCon.delete)
+        delBtn.grid(row=0, column=3)
+        delBtn = tkinter.Button(btnPane, text='edit selected', command=lambda: playcon.editselect(
+            [self.recordingBox.get(idx) for idx in self.recordingBox.curselection()]))
         delBtn.grid(row=0, column=4)
-        delBtn = tkinter.Button(btnPane, text='edit selected',command=lambda : playcon.editselect([self.recordingBox.get(idx) for idx in self.recordingBox.curselection()]))
-        delBtn.grid(row=0, column=5)
 
     def bottomPage(self):
         bottomframe = tkinter.Frame(self)
@@ -70,13 +73,8 @@ class mainPage(tkinter.Tk):
         label.pack()
 
 
-    def editPageClosed(self):
-        self.editing = False
-
-
 
 
 if __name__ == '__main__':
-    playcon=playControls()
+    playcon = playControls()
     main = mainPage(playcon)
-    main.mainloop()
