5666ec28a0b7a50e3b4c7cc80637e7fba70c3126
jeron <slamjeron@gmail.com>
2018-10-26 15:54:43 -0600

    save and open files works i use a file type caled .grind to hold my information i still need to add the open recent.

--- autoGrind/Gui2/otherComand.py
+++ autoGrind/Gui2/otherComand.py
@@ -13,12 +13,18 @@ class editPage(tkinter.Frame):
                                         'Drag Stop', 'Type', 'Color Stall', 'Color Condition', 'name groop')
 
         recVar.set('Color Stall')
+        # there will be a dellay entry for the second
         actionDrop.grid(row=0,column=0)
         actionDrop.config(width=20)
+        delaylab=tkinter.Label(parent,text='stall time ')
+        delayAmount = tkinter.Spinbox(parent,from_=0,to=100000)
+        delayAmount.grid(row=1,column = 1)
+        delayAmount.config(width=10)
 
 
 
 if __name__ == '__main__':
     playcon = tkinter.Tk()
     edit = editPage(playcon)
+
     edit.mainloop()
